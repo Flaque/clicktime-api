53c6c700d70eb94bed53a431af4da214909f9a1b
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-05 13:59:14 -0700

    Add loading data from server into autocomplete

--- src/ui/TaskWidget/TaskWidget.jsx
+++ src/ui/TaskWidget/TaskWidget.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import Search from '../search/Search.jsx'
+import {getAllTasks} from '../../network/ClickTimeAPI/ClickTimeAPI';
 
-const items = [{name: 'hi', key: 213}, {name: 'hello', key: 233}]
+const getNameIdPairs = function(taskDict) {
+  let pairs = []
+  for (let id in taskDict) {
+    pairs.push({
+      name: taskDict[id].Name,
+      id: id
+    })
+  }
+  return pairs
+}
+
 
 class TaskWidget extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { tasks: {}, namePairs: [] }
+  }
+
+  componentDidMount() {
+    const creds = this.props.credentials
+    getAllTasks(creds.companyID, creds.userID)
+      .then( (data) => {
+        this.setState({tasks: data, namePairs: getNameIdPairs(data) })
+       })
+  }
+
   render() {
     return (
       <div className="taskWidget card">
         <h3> Tasks </h3>
-        <Search items={ items }/>
+        <Search items={ this.state.namePairs }/>
       </div>
     )
   }
