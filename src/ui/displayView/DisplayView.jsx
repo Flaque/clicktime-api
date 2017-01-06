8d9440ccd290a74b4f4769175a3634f0aac64a9f
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-06 14:07:01 -0700

    Refactor displayview

--- src/ui/displayView/DisplayView.jsx
+++ src/ui/displayView/DisplayView.jsx
@@ -2,39 +2,65 @@ import React from 'react';
 
 class DisplayView extends React.Component {
 
-  renderJobs() {
-    if (!this.props.selectedTask.jobs) {
-      return ( <p> Nothing! </p>)
-    }
-
-    return this.props.selectedTask.jobs.map( (job) =>
-      <div className="row content" key={job.JobID} >
+  /**
+   * Renders Job/Client headers
+   */
+  renderHeaders() {
+    return (
+      <div className="row">
         <div className="six columns">
-          {job.DisplayName}
+          <h6 className="upper light"> Job </h6>
         </div>
         <div className="six columns">
-          {job.client.DisplayName}
+          <h6 className="upper light"> Client </h6>
         </div>
       </div>
+    )
+  }
+
+  /**
+   * Renders a job row for each job
+   */
+  renderJobs() {
+    return this.props.selectedTask.jobs.map( (job) =>
+        <div className="row content" key={job.JobID} >
+          <div className="six columns">
+            {job.DisplayName}
+          </div>
+          <div className="six columns">
+            {job.client.DisplayName}
+          </div>
+        </div>
+    )
+  }
+
+  /**
+   * Renders both the headers and jobs
+   */
+  renderContent() {
+    return (
+      <div>
+        {this.renderHeaders()}
+        {this.renderJobs()}
+      </div>
+    )
+  }
+
+  /**
+   * Renders a prompt if nothing is entered yet.
+   */
+  renderEmpty() {
+    return (
+      <div className="empty-warning"> Type in a task! </div>
     )
   }
 
   render() {
     return (
       <div className="displayView">
-
-        {/* Headers */}
-        <div className="row">
-          <div className="six columns">
-            <h6 className="upper light"> Job </h6>
-          </div>
-          <div className="six columns">
-            <h6 className="upper light"> Client </h6>
-          </div>
-        </div>
-
-        {this.renderJobs()}
-
+        {this.props.selectedTask.jobs
+            ? this.renderContent()
+            : this.renderEmpty()}
       </div>
     )
   }
