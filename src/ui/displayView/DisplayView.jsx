7fdc6be79a65cce14a7dee0091fa2fac2f4ea06e
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-05 21:45:17 -0700

    Remove "recent" column
    It doesn't appear that this is relevant to the user at this point.

--- src/ui/displayView/DisplayView.jsx
+++ src/ui/displayView/DisplayView.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 class DisplayView extends React.Component {
 
-  constructor(props) {
-    super(props)
-    console.log(this.props.selectedTask)
-  }
-
   renderJobs() {
     if (!this.props.selectedTask.jobs) {
       return ( <p> Nothing! </p>)
@@ -14,14 +9,11 @@ class DisplayView extends React.Component {
 
     return this.props.selectedTask.jobs.map( (job) =>
       <div className="row content" key={job.JobID} >
-        <div className="four columns">
+        <div className="six columns">
           {job.DisplayName}
         </div>
-        <div className="four columns">
+        <div className="six columns">
           BMW
-        </div>
-        <div className="four columns">
-          ❌
         </div>
       </div>
     )
@@ -33,14 +25,11 @@ class DisplayView extends React.Component {
 
         {/* Headers */}
         <div className="row">
-          <div className="four columns">
+          <div className="six columns">
             <h6 className="upper light"> Job </h6>
           </div>
-          <div className="four columns">
+          <div className="six columns">
             <h6 className="upper light"> Client </h6>
-          </div>
-          <div className="four columns">
-            <h6 className="upper light"> Recent? </h6>
           </div>
         </div>
 
